3823168d23ce345de19ce4ad124f0c3d5d9dc0b3
lsundal1 <126904972+lsundal1@users.noreply.github.com>
2024-10-14 13:59:05 +0100

    create a test for endpoint /api which responds with an object detailing all available endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,7 @@ const request = require('supertest')
 const db = require('../db/connection.js')
 const seed = require('../db/seeds/seed.js')
 const { articleData, topicData, userData, commentData } = require('../db/data/test-data/index.js')
-
-// require('jest-sorted')
+const endpoints = require('../endpoints.json')
 
 beforeEach(() => {
     return seed({ articleData, topicData, userData, commentData })
@@ -14,24 +13,33 @@ afterAll(() => {
 })
 
 describe('GET:/api/topics', () => {
-    test('GET: 200 - sends a list of topics to the client', () => {
-        return request(app)
-            .get('/api/topics')
-            .expect(200)
-            .then(({body}) => {
-                console.log(body.topics)
-                expect(body.topics.forEach(topic => {
-                    expect(typeof topic.description).toBe('string')
-                    expect(typeof topic.slug).toBe('string')
-                }))
-            })
+test('GET: 200 - sends a list of topics to the client', () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then(({body}) => {
+            expect(body.topics.forEach(topic => {
+                expect(typeof topic.description).toBe('string')
+                expect(typeof topic.slug).toBe('string')
+            }))
+        })
     })
     test('GET: 404 - returns 404 if endpoint does not exist', () => {
         return request(app)
-            .get('/api/giraffes')
-            .expect(404)
-            .then((response) => {
-                expect(response.res.statusMessage).toBe('Not Found')
-            })
+        .get('/api/giraffes')
+        .expect(404)
+        .then((response) => {
+            expect(response.res.statusMessage).toBe('Not Found')
+        })
+    })
+})
+describe.only('GET:/api', () => {
+    test('GET:200 - responds with an object detailing all available endpoints', () => {
+        return request(app)
+            .get('/api')
+            .expect(200)
+            .then(({body}) => {
+                expect(body.endpoints).toEqual(endpoints)
+            }) 
     })
 })
