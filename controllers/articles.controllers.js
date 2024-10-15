852bf49875df4a62e0c8bce5fd1775a5670d9921
lsundal1 <126904972+lsundal1@users.noreply.github.com>
2024-10-15 10:45:38 +0100

    added testing for /api/articles, error handling is provided by 404 - bad request

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,10 +1,20 @@
-const { fetchArticles } = require('../models/articles.models')
+const { fetchArticles, fetchArticlesById } = require('../models/articles.models')
+
+
+exports.getArticlesById = (req,res,next) => {
+    
+    const { article_id } = req.params
+    
+    fetchArticlesById(article_id).then((article) => {
+        res.status(200).send({article})
+    })
+    .catch(next)
+}
 
 exports.getArticles = (req,res,next) => {
-        
-    const { article_id } = req.params
-    fetchArticles(article_id).then((article) => {
-        res.status(200).send({article})
+    
+    fetchArticles().then((articles) => {
+        res.status(200).send({articles})
     })
     .catch(next)
 }
